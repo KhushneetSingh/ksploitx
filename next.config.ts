5a306f2517f9162bb17c9ceca9d8ef1a25745a40
Khushneet Singh <singhkhushneet601@gmail.com>
2026-04-30 16:00:28 +0530

    fix: image not loading in the deployment

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  allowedDevOrigins: ['192.168.1.11'],
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.jsdelivr.net",
-        pathname: "/gh/devicons/devicon@latest/icons/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
