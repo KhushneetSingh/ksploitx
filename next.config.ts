87637856c9803a23121d7f90d20bb4c77e2419ad
Khushneet Singh <singhkhushneet601@gmail.com>
2026-05-12 20:12:34 +0530

    fix: add dynamic basepath for Github pages deployments

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repoName = '';
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repoName = `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: repoName,
+  assetPrefix: repoName,
 };
 
 export default nextConfig;
